e0e66ba8a4f5e1e3c3ca30df41931abe732c57c3
skigth <fmjgomes@gmail.com>
2017-03-09 11:37:29 +0000

    more keypair logic added

--- src/ws/ClientManager.java
+++ src/ws/ClientManager.java
@@ -12,23 +12,37 @@ import java.security.KeyPairGenerator;
 import java.security.*;
 import java.security.spec.X509EncodedKeySpec;
 import java.io.*;
+import java.util.*;
+import java.security.cert.Certificate;
+
 
 public class ClientManager{
+
+	//
+	
+
+	public void init(KeyStore ks){}
+
+
     public void register_user(){
+    	String password = "password";
+    	char[] pass = password.toCharArray();
+    	
         try{
         KeyPairGenerator kgen = KeyPairGenerator.getInstance("RSA");
         kgen.initialize(1024);
         KeyPair pair = kgen.generateKeyPair();
-        PrivateKey priv = pair.getPrivate();
-        savePrivateKey(priv);
-        PublicKey pub = pair.getPublic();
-        savePublicKey(pub);
+        
+        saveKeypair(pair,pass);
+        //PublicKey pub = pair.getPublic();
+        //savePublicKey(pub);
         }
         catch(Exception e){}  
 
        
                
     }
+    /*
     public void savePublicKey(PublicKey pub){
 		try{
 		byte[] pubEnc = pub.getEncoded();
@@ -38,16 +52,58 @@ public class ClientManager{
 		}
 		catch(Exception e){}
     }
-    public void savePrivateKey(PrivateKey priv){
+
+*/
+
+
+
+
+
+    public void saveKeypair(KeyPair pair, char [] password){
+    	
     	try{
-		byte[] privEnc = priv.getEncoded();
-		FileOutputStream fos = new FileOutputStream("." + "/private.key");
-		fos.write(privEnc);
-		fos.close();
-		}
-		catch(Exception e){}
+    		KeyStore keystore = openKeystore("password".toCharArray());
+
+    		//PrivateKey priv = pair.getPrivate();
+
+    	   
+		    
+
+		    String ali = "mykeypair";
+
+		    Key key = keystore.getKey(ali, password);
+			keystore.setCertificateEntry(ali, cert);
+    	 if ( key instanceof PrivateKey) {
+
+		      // Get certificate of public key
+		      Certificate cert = keystore.getCertificate(ali);
+
+		      // Get public key
+		      PublicKey publicKey = cert.getPublicKey();
+
+		      // Return a key pair
+		      new KeyPair(publicKey, (PrivateKey) key);
+    	}
+    	}catch(Exception e){}
+ /*
+		ks.load(null,password);
+ 		
+
+    	//Create a new file to store the KeyStore object
+		java.io.FileOutputStream fos = new;
+		java.io.FileOutputStream("keystorefile.jce");
+
+		//Write the KeyStore into the file
+		ks.store(fos, "Keys".toCharArray());
+		//Close the file stream
+		fos.close(); 
+		*/
     }
 
+ 	
+
+
+/*
     public Key getPublic(){
     	Key pubKey = null;
     	try{
@@ -71,7 +127,50 @@ public class ClientManager{
 		}
 		return pubKey;
     
+	}*/
+
+	public void createKeystore(char [] keystorePassword){
+		try{
+		// Create an instance of KeyStore of type “JCEKS”.
+		 // JCEKS refers the KeyStore implementation from SunJCE provider
+		KeyStore ks = KeyStore.getInstance("JCEKS");
+		 // Load the null Keystore and set the password to keyStorepassword
+		 ks.load(null, keystorePassword); 
+		 /*
+		 //add password protectio to keystore
+		KeyStore.PasswordProtection password;
+		password = new KeyStore.PasswordProtection(keystorePassword); 
+		*/
+		//Create a new file to store the KeyStore object
+		java.io.FileOutputStream fos = new java.io.FileOutputStream("keystorefile.jce");
+
+		//Write the KeyStore into the file
+		ks.store(fos, keystorePassword);
+		//Close the file stream
+		fos.close(); 
+
+	}catch(Exception e){}
 	}
 
+	public KeyStore openKeystore(char [] keystorePassword){
+		try{
+		//Open the KeyStore file
+		FileInputStream fis = new FileInputStream("keystorefile.jce"); 
+		//Create an instance of KeyStore of type “JCEKS”
+		KeyStore ks = KeyStore.getInstance("JCEKS");
+		//Load the key entries from the file into the KeyStore object.
+		ks.load(fis, keystorePassword);
+		fis.close();
+		//Get the key with the given alias.
+		
+		//String alias=args[0];
+		//Key k = ks.getKey(alias, "changeme".toCharArray()); 
+		return ks;
+		
+		}catch(Exception e){}	
+		return null;
+		}
 
 }
+
+
