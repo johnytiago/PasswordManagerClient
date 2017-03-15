1b3c83a755ffab061af04ac84975765b2a4f477d
johnytiago <joaosao10@gmail.com>
2017-03-15 15:13:55 +0000

    Adds Connect & wsClient methods return String now  Also: `mv src/* src/java` to make run for tests folder

---
+++ src/java/ws/PasswordManagerWSClient.java
@@ -0,0 +1,36 @@
+package ws;
+
+import ws.PasswordManagerWS;
+import ws.PasswordManagerWSImplService;
+
+public class PasswordManagerWSClient {
+
+  private static PasswordManagerWS _passwordmanagerWS;
+  
+  public static void main(String[] args) {
+	  connect();
+  }
+  
+  public static void connect() {
+	  PasswordManagerWSImplService pmWSImplService = new PasswordManagerWSImplService();
+	  _passwordmanagerWS = pmWSImplService.getPasswordManagerWSImplPort();
+	  
+	  System.out.println("Connected to PasswordManagerServer.");
+	  System.out.println("Found service running at: " + pmWSImplService.getWSDLDocumentLocation().toString());
+  }
+  
+  public String get(byte[] pubKey, byte[] domain, byte[] username) {
+	  return _passwordmanagerWS.get(pubKey, domain, username);
+	  // check if res equals error ?
+  }
+  
+  public String put(byte[] pubKey, byte[] domain, byte[] username, byte[] password) {
+	 return _passwordmanagerWS.put(pubKey, domain, username, password);
+	  // check if res equals error ?
+  }
+  
+  public String register(byte[] pubKey) {
+	  return _passwordmanagerWS.register(pubKey);
+	  // check if res equals error ?
+  }
+}
