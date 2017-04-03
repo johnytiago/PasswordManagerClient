7bab2e05333e5fabd653d1790a697a0d6e87ced1
snackk <santos_matrix@sapo.pt>
2017-04-03 21:05:28 +0100

    Get & Put & Request uses envelop to send data.

--- src/java/ws/PasswordManagerWSClient.java
+++ src/java/ws/PasswordManagerWSClient.java
@@ -1,5 +1,7 @@
 package ws;
 
+import exception.PasswordManagerExceptionHandler;
+
 public class PasswordManagerWSClient {
 
   private static PasswordManagerWS _passwordmanagerWS;
@@ -17,18 +19,59 @@ public class PasswordManagerWSClient {
 	  System.out.println("Found service running at: " + pmWSImplService.getWSDLDocumentLocation().toString());
   }
   
-  public byte[] get(Envelope envelope) {
-	  return _passwordmanagerWS.get(envelope);
-	  // check if res equals error ?
+  public byte[] get(byte[] pubKey, byte[] domain, byte[] username) {
+	  Envelope envelope = new Envelope();
+		
+	  Message msg = new Message();
+	  msg.setPublicKey(pubKey);
+	  msg.setDomain(domain);
+	  msg.setUsername(username);
+	  
+	  envelope.setMessage(msg);
+	  try{
+		  return _passwordmanagerWS.get(envelope);
+	  }
+	  catch(PasswordManagerExceptionHandler pme){
+		  return null;
+	  }
   }
   
-  public void put(Envelope envelope) {
-	  _passwordmanagerWS.put(envelope);
+  public String put(byte[] pubKey, byte[] domain, byte[] username, byte[] password) {
+	  
 	  // check if res equals error ?
+	  
+	  Envelope envelope = new Envelope();
+		
+	  Message msg = new Message();
+	  msg.setPublicKey(pubKey);
+	  msg.setDomain(domain);
+	  msg.setUsername(username);
+	  msg.setPassword(password);
+	  
+	  envelope.setMessage(msg);
+	  try{
+		  _passwordmanagerWS.put(envelope);
+	  }
+	  catch(PasswordManagerExceptionHandler pme){
+		  return pme.getMessage();
+	  }
+	  return "";
   }
   
-  public void register(Envelope envelope) {
-	  _passwordmanagerWS.register(envelope);
+  public String register(byte[] pubKey) {
+	  Envelope envelope = new Envelope();
+	
+	  Message msg = new Message();
+	  msg.setPublicKey(pubKey);
+	  
+	  envelope.setMessage(msg);
+	  try{
+		  _passwordmanagerWS.register(envelope);
+	  }
+	  catch(PasswordManagerExceptionHandler pme){
+		  return pme.getMessage();
+	  }
+	  return "";
 	  // check if res equals error ?
   }
 }
