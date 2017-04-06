fceeb749e8d0b26a2c325f2f0e281f67d07dfdcc
johnytiago <joaosao10@gmail.com>
2017-04-06 08:47:33 +0100

    Fixes identation

--- src/java/ws/PasswordManagerWSClient.java
+++ src/java/ws/PasswordManagerWSClient.java
@@ -8,36 +8,36 @@ public class PasswordManagerWSClient {
 
 
   public PasswordManagerWSClient() {
-	  connect();
+    connect();
   }
-  
+
   private static void connect() {
-	  PasswordManagerWSImplService pmWSImplService = new PasswordManagerWSImplService();
-	  _passwordmanagerWS = pmWSImplService.getPasswordManagerWSImplPort();
-	  
-	  System.out.println("Connected to PasswordManagerServer.");
-	  System.out.println("Found service running at: " + pmWSImplService.getWSDLDocumentLocation().toString());
+    PasswordManagerWSImplService pmWSImplService = new PasswordManagerWSImplService();
+    _passwordmanagerWS = pmWSImplService.getPasswordManagerWSImplPort();
+
+    System.out.println("Connected to PasswordManagerServer.");
+    System.out.println("Found service running at: " + pmWSImplService.getWSDLDocumentLocation().toString());
   }
-  
+
   public byte[] get(byte[] pubKey, byte[] domainHash, byte[] usernameHash) {
-	  Envelope envelope = new Envelope();
-		
-	  Message msg = new Message();
-	  msg.setPublicKey(pubKey);
-	  msg.setDomainHash(domainHash);
-	  msg.setUsernameHash(usernameHash);
-	  
-	  envelope.setMessage(msg);
-	  try{
-		  return _passwordmanagerWS.get(envelope);
-	  }
-	  catch(PasswordManagerExceptionHandler pme){
-		  return null;
-	  }
+    Envelope envelope = new Envelope();
+
+    Message msg = new Message();
+    msg.setPublicKey(pubKey);
+    msg.setDomainHash(domainHash);
+    msg.setUsernameHash(usernameHash);
+
+    envelope.setMessage(msg);
+    try{
+      return _passwordmanagerWS.get(envelope);
+    }
+    catch(PasswordManagerExceptionHandler pme){
+      return null;
+    }
   }
-  
+
   public String put(byte[] pubKey, byte[] domainHash, byte[] usernameHash, byte[] password, byte[] tripletHash, ) {
-	  
+
     Message msg = new Message();
     msg.setPublicKey( pubKey);
     msg.setDomainHash(domainHash);
@@ -46,31 +46,31 @@ public class PasswordManagerWSClient {
     msg.setPassword(password);
 
     Envelope envelope = new Envelope();
-	  envelope.setMessage(msg);
-      
-	  try{
-		  _passwordmanagerWS.put(envelope);
-		  return "";
-	  }
-	  catch(PasswordManagerExceptionHandler pme){
-		  return pme.getMessage();
-	  }
+    envelope.setMessage(msg);
+
+    try{
+      _passwordmanagerWS.put(envelope);
+      return "";
+    }
+    catch(PasswordManagerExceptionHandler pme){
+      return pme.getMessage();
+    }
   }
-  
+
   public String register(byte[] pubKey) {
 
-	  Message msg = new Message();
-	  msg.publicKey = pubKey;
-	  Envelope envelope = new Envelope();
-	  envelope.message = msg;
+    Message msg = new Message();
+    msg.publicKey = pubKey;
+    Envelope envelope = new Envelope();
+    envelope.message = msg;
 
-	  try{
-		  _passwordmanagerWS.register(envelope);
+    try{
+      _passwordmanagerWS.register(envelope);
       return "";
-	  }
-	  catch(PasswordManagerExceptionHandler pme){
-		  return pme.getMessage();
-	  }
-	  // check if res equals error ?
+    }
+    catch(PasswordManagerExceptionHandler pme){
+      return pme.getMessage();
+    }
+    // check if res equals error ?
   }
 }
