52ddae729c154aeead06558fd70cf88d2252756e
skigth <fmjgomes@gmail.com>
2017-04-28 17:51:21 +0100

    Most of the Counter implemented some dougths

--- src/java/ws/PasswordManagerWSClient.java
+++ src/java/ws/PasswordManagerWSClient.java
@@ -90,16 +90,17 @@ public class PasswordManagerWSClient {
     byte[] domainHash = _crypto.genSign(_util.addSalt(domain.getBytes(),salt), (PrivateKey)_crypto.getPrivateKey());
     byte[] usernameHash = _crypto.genSign(_util.addSalt(username.getBytes(),salt), (PrivateKey)_crypto.getPrivateKey());
     int counter;
-    /*
-    if(!counters.containsKey(_crypto.publickeyToString(pubKey))){
-    	_crypto.checkCounter(counters,_crypto.publickeyToString(pubKey));
-    	}*/
-    
+    if(checkCounter(_util.publickeyToString(pubKey),counter)){
+    	counter = getCounter(_util.publickeyToString(pubKey));
+    }
+    else{
+    	//TODO: send some kind of exception
+    }
     Message msg = new Message();
     msg.setPublicKey(pubKey);
     msg.setDomainHash(domainHash);
     msg.setUsernameHash(usernameHash);
-    /*msg.setCounter(checkcounter);*/
+    msg.setCounter(counter); //why do the prepare envelope and also set the counter here=? weird
 
     Envelope envelope = new Envelope();
     envelope.setMessage(msg);
@@ -141,13 +142,14 @@ public class PasswordManagerWSClient {
     byte[] usernameHash = _crypto.genSign(_util.addSalt(username.getBytes(),salt), (PrivateKey)_crypto.getPrivateKey());
     byte[] encryptedPassword = _crypto.encrypt(password.getBytes(), _crypto.getPublicKey());
     byte[] tripletHash = _crypto.signTriplet(domainHash, usernameHash, encryptedPassword, (PrivateKey)_crypto.getPrivateKey());
-
+    
     Message msg = new Message();
     msg.setPublicKey( pubKey);
     msg.setDomainHash(domainHash);
     msg.setUsernameHash(usernameHash);
     msg.setTripletHash(tripletHash);
     msg.setPassword(encryptedPassword);
+
 
     Envelope envelope = new Envelope();
     envelope.setMessage(msg);
@@ -173,6 +175,11 @@ public class PasswordManagerWSClient {
     byte[] HMAC = _crypto.genMac( _util.msgToByteArray( envelope.getMessage() ), DHSecretKey );
     return Arrays.equals(HMAC, envelope.getHMAC());
   }
+  private boolean verifyCounter(Envelope envelope){
+	  int counter = envelope.getMessage().getCounter();
+	  byte [] pub = envelope.getDHPublicKey(); //Not sure if it is DHkey that we want to check here
+	  return checkCounter(_util.publickeyToString(pub),counter);
+  }
 
   private void addHMAC( Envelope envelope ) {
     DHSecretKey = _crypto.generateDH( _crypto.getDHPrivateKey(), DHPubKeySrv);
@@ -180,16 +187,57 @@ public class PasswordManagerWSClient {
     envelope.setHMAC( HMAC );
     return;
   }
+  private void addCounter(String pub, Envelope envelope){
+	  envelope.getMessage().setCounter(getCounter(pub));
+  }
 
   private void prepareEnvelope( Envelope envelope ) {
     envelope.setDHPublicKey( _crypto.getDHPublicKey().getEncoded() );
     addHMAC( envelope );
-    // TODO: counter
+    addCounter(_util.publickeyToString(_crypto.getDHPublicKey().getEncoded()),envelope);//not sure if this is the pubkey to use
     return;
   }
 
   private boolean verifyEnvelope( Envelope envelope ) {
-    // TODO: Counter
-    return verifyHMAC( envelope ); // && verifyCounter( envelope );
+    
+    return verifyHMAC( envelope )&&verifyCounter(envelope); // && verifyCounter( envelope );
   }
+  
+  
+	public boolean checkCounter(String pub,int clientCounter){
+		if(counters.containsKey(pub)){
+			if(counters.get(pub)<clientCounter){
+			counters.put(pub,clientCounter+1);// update counter value after checking it
+			return true;
+			}else{
+			//throw new CounterIncorrectException();
+				return false;
+			}
+		}
+
+			initCounter(pub);
+			return true;
+		
+	}
+	
+	public int incrementCounter(int counter){
+		return counter+1;
+	}
+	
+	public int initCounter(String pub){
+		if(!counters.containsKey(pub)){
+			int  value = (int )(Math.random() * 10000);
+			counters.put(pub, value);
+			return value;
+		}
+		else{
+			return counters.get(pub);
+			//throw exception?
+			
+		}
+		}
+	public int getCounter(String pub){
+		return counters.get(pub);
+	}
+	
 }
